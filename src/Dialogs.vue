41be217ee9906237d0708d4e52d2559c42c5d363
Trevor <tdola@bisontransport.com>
2019-11-26 14:44:35 -0600

    Added enter key for dismissing dialog

--- src/Dialogs.vue
+++ src/Dialogs.vue
@@ -1,6 +1,9 @@
 <template lang="pug">
 .v-dialogs
   v-dialog(:show='item.show' v-for='item in items', :item='item', @close='onClose', :key='item.key')
+
+
+
 </template>
 
 <script>
@@ -10,13 +13,13 @@ import VDialog from './Dialog'
 
 export default {
   components: { VDialog },
-  data() {
+  data () {
     return {
       items: []
     }
   },
   methods: {
-    open( message, { title, cancelLabel, prompt, size, okLabel = 'OK' }) {
+    open (message, { title, cancelLabel, prompt, size, okLabel = 'OK' }) {
       if (!this.$parent) {
         this.$mount()
         document.body.appendChild(this.$el)
@@ -31,53 +34,60 @@ export default {
           okLabel,
           prompt,
           size,
-          resolve
+          resolve,
+          type
         }
         this.items.push(item)
       })
     },
 
-    alert(message, options = {}) {
+    alert (message, options = {}) {
       const { title, okLabel = 'OK', size } = options
-      return this.open( message, { title, okLabel, size })
+      return this.open(message, { title, okLabel, size, type: 'alert' })
     },
 
-    confirm(message, options = {}) {
+    confirm (message, options = {}) {
       const { title, cancelLabel = 'Cancel', okLabel = 'OK', size } = options
-      return this.open( message, { title, cancelLabel, okLabel, size })
+      return this.open(message, { title, cancelLabel, okLabel, size, type: 'confirm' })
     },
 
-    prompt(message, options = {}) {
+    prompt (message, options = {}) {
       let { title, okLabel = 'OK', size, prompt } = options
       prompt = Object.assign({ value: '', invalidMessage: 'invalid!', component: Prompt }, prompt)
-      return this.open(message, { title, okLabel, prompt, size })
+      return this.open(message, { title, okLabel, prompt, size, type: 'prompt' })
     },
 
-    remove(item) {
+    remove (item) {
       let i = this.items.indexOf(item)
       if (i >= 0) {
         this.items.splice(i, 1)
       }
     },
 
-    onClose(item, ok) {
-      const response = {ok}
+    onClose (item, ok) {
+      const response = { ok }
       if (item.prompt) response.value = item.prompt.value
       item.resolve(response)
       item.show = false
       this.remove(item)
     },
 
-    keyUp(e) {
+    keyUp (e) {
       if ('Escape' === e.key) {
         if (this.items.length > 0) {
+          this.onClose(this.items[this.items.length - 1])
+        }
+      } else if ('Enter' === e.key) {
+        // If you pressed enter (Windows)
+        if (!this.items[this.items.length - 1].prompt) {
+          // If it's not a prompt, then hitting enter is allowed for closing
           this.onClose(this.items[this.items.length - 1])
         }
       }
     }
   },
 
-  created() {
+  created () {
     window.addEventListener('keyup', this.keyUp)
   }
 }
